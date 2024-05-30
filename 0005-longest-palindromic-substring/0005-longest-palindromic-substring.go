5d8cc1ef47e8636c36db72bea5f1d0550ddb521d
Kompiang Gede Sukadharma <51501141+kompiangg@users.noreply.github.com>
2024-05-31 05:33:50 +0800

    Time: 44 ms (39.27%), Space: 2.7 MB (45.24%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -0,0 +1,23 @@
+func longestPalindrome(s string) string {
+    for windowLen := len(s); windowLen >= 0; windowLen-- {
+        for startIdx := 0; startIdx <= len(s) - windowLen; startIdx++ {
+            if isPalindrom(s[startIdx:startIdx + windowLen]) {
+                return s[startIdx:startIdx + windowLen]
+            }
+        }
+    }
+    
+    return string(s[0])
+}
+
+func isPalindrom(str string) bool {
+    strLen := len(str)
+    
+    for i := 0; i < strLen/2; i++ {
+        if str[i] != str[strLen - i - 1] {
+            return false
+        }
+    }
+    
+    return true
+}
